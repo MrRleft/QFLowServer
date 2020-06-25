fe47836706ad5edd450690543b36ef56a8813a64
Victor <vigomezj@ucm.es>
2020-06-25 18:06:13 +0200

    Fix one query

--- src/main/java/com/qflow/server/domain/repository/InfoUserQueueRepository.java
+++ src/main/java/com/qflow/server/domain/repository/InfoUserQueueRepository.java
@@ -13,7 +13,9 @@ public interface InfoUserQueueRepository extends JpaRepository<InfoUserQueueDB, 
     @Query(value = "SELECT * " +
             "FROM info_user_queue" +
             " WHERE id_user_iuq_fk = :id_user_iuq_fk" +
-            " AND id_queue_iuq_fk = :id_queue_iuq_fk ",
+            " AND id_queue_iuq_fk = :id_queue_iuq_fk " +
+            "ORDER BY id DESC " +
+            "LIMIT 1",
             nativeQuery = true)
     Optional<InfoUserQueueDB> getUserInInfoUserQueue(@Param("id_user_iuq_fk") int idUser,
                                          @Param("id_queue_iuq_fk") int idQueue);
